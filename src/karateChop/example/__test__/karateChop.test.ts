35c33625f5042bfce9420934ca7566df2f05f30c
ShotaroSuzu <cosmos.1220.3110@gmail.com>
2022-04-06 21:55:34 +0900

    add: unimplemented example of karate chop

---
+++ src/karateChop/example/__test__/karateChop.test.ts
@@ -0,0 +1,27 @@
+import chop from './../karateChop';
+describe(`Karate Chop`, () => {
+  describe('sample tests', () => {
+    it(`should equals`, () => {
+      expect(chop(3, [])).toEqual(-1);
+
+      expect(chop(3, [1])).toEqual(-1);
+      expect(chop(1, [1])).toEqual(0);
+      expect(chop(1, [1, 3, 5])).toEqual(0);
+      expect(chop(3, [1, 3, 5])).toEqual(1);
+      expect(chop(5, [1, 3, 5])).toEqual(2);
+      expect(chop(0, [1, 3, 5])).toEqual(-1);
+      expect(chop(2, [1, 3, 5])).toEqual(-1);
+      expect(chop(4, [1, 3, 5])).toEqual(-1);
+      expect(chop(6, [1, 3, 5])).toEqual(-1);
+      expect(chop(1, [1, 3, 5, 7])).toEqual(0);
+      expect(chop(3, [1, 3, 5, 7])).toEqual(1);
+      expect(chop(5, [1, 3, 5, 7])).toEqual(2);
+      expect(chop(7, [1, 3, 5, 7])).toEqual(3);
+      expect(chop(0, [1, 3, 5, 7])).toEqual(-1);
+      expect(chop(2, [1, 3, 5, 7])).toEqual(-1);
+      expect(chop(4, [1, 3, 5, 7])).toEqual(-1);
+      expect(chop(6, [1, 3, 5, 7])).toEqual(-1);
+      expect(chop(8, [1, 3, 5, 7])).toEqual(-1);
+    });
+  });
+});
